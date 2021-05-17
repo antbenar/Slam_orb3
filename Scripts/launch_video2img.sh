98cee93fe322615ac2474ec63463814ba9906dce
antbenar <antbenar1999@gmail.com>
2021-05-16 23:59:49 -0500

    Resultados

--- Scripts/launch_video2img.sh
+++ Scripts/launch_video2img.sh
@@ -4,4 +4,6 @@
 echo "Convirtiendo video a una sequencia de frames .png"
 #python3 video2img.py ../Datasets/TEST/mav0/cam0/data_video/XiaomiMiA3.mp4 ../Datasets/TEST/mav0/cam0/data/ 30 
 
-python3 video2img.py ../Datasets/TEST_3/mav0/cam0/data_video/XiaomiMiA3.mp4 ../Datasets/TEST_3/mav0/cam0/data/ 20 ../Datasets/TEST_3/mav0/cam0/data.csv
+#python3 video2img.py ../Datasets/TEST_3/mav0/cam0/data_video/XiaomiMiA3.mp4 ../Datasets/TEST_1/mav0/cam0/data/ 20 ../Datasets/TEST_1/mav0/cam0/data.csv
+
+python3 video2img.py "../Datasets/TEST_1/mav0/cam0/data_video/Xiaomi Mi A3 Video 2 720x1280.mp4" ../Datasets/TEST_1/mav0/cam0/data/ 20 ../Datasets/TEST_1/mav0/cam0/data.csv
--- run_ORB_SAM3.sh
+++ run_ORB_SAM3.sh
@@ -4,6 +4,9 @@ pathDatasetEuroc="$PWD/Datasets" #Example, it is necesary to change it by the da
 #------------------------------------
 # Monocular Examples
 echo "Launching MH01 with Monocular sensor"
-#./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml "$pathDatasetEuroc/TEST" ./Datasets/TEST/mav0/timestamp.txt dataset-test_robotica
 
-./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml "$pathDatasetEuroc/TEST_3" ./Datasets/TEST_3/mav0/timestamp.txt dataset-test_3_robotica
+#./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml "$pathDatasetEuroc/TEST_3" ./Datasets/TEST_3/mav0/timestamp.txt dataset-test_3_robotica
+
+#./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml "$pathDatasetEuroc/TEST_2" ./Datasets/TEST_2/mav0/timestamp.txt dataset-test_2_robotica
+
+./Monocular/mono_euroc ../Vocabulary/ORBvoc.txt ./Monocular/EuRoC.yaml "$pathDatasetEuroc/TEST_1" ./Datasets/TEST_1/mav0/timestamp.txt dataset-test_1_robotica
